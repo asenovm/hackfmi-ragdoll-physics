28aa168e6c8ee7e0de48d4437127b1d331bd0f7f
asenovm <asenov.m@gmail.com>
2014-11-07 23:20:29 +0200

    * Updated menu activity UI

--- BubbleCombat/src/com/midtownmadness/bubblecombar/model/MockGamesAdapter.java
+++ BubbleCombat/src/com/midtownmadness/bubblecombar/model/MockGamesAdapter.java
@@ -1,7 +1,5 @@
 package com.midtownmadness.bubblecombar.model;
 
-import java.util.Arrays;
-
 import android.content.Context;
 
 import com.midtownmadness.bubblecombat.R;
@@ -9,8 +7,10 @@ import com.midtownmadness.bubblecombat.R;
 public class MockGamesAdapter extends GamesAdapter {
 
 	public MockGamesAdapter(final Context context) {
-		super(context, R.id.game_view_text, Arrays.asList(
+		super(context, R.id.game_view_text, new GameModel[] {
 				new GameModel("Game One"), new GameModel("Game Two"),
-				new GameModel("Game Three")).toArray(new GameModel[0]));
+				new GameModel("Game Three"), new GameModel("Game Four"),
+				new GameModel("Game Five"), new GameModel("Game Six"),
+				new GameModel("Game Seven"), new GameModel("Game Eight") });
 	}
 }
