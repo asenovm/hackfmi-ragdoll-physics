ab58cab98842f46f6e0a10ac8076f87b51bb178c
GeorgiPachov <georgi.patchov@gmail.com>
2014-11-08 19:09:49 +0200

    Added comm object #1

---
+++ BubbleCombat/src/com/midtownmadness/bubblecombat/multiplay/commobjects/GoMessageObject.java
@@ -0,0 +1,9 @@
+package com.midtownmadness.bubblecombat.multiplay.commobjects;
+
+public class GoMessageObject {
+	public final long timestamp;
+
+	public GoMessageObject(long timestamp){
+		this.timestamp = timestamp;
+	}
+}
